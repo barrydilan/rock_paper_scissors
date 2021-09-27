eecc87a71952bcb737b15b67fb9fc364a6b2c29c
HarryH <queharambe@gmail.com>
2021-09-27 19:26:34 +0300

    -basic algorithm/no DOM manipuations

--- script/script.js
+++ script/script.js
@@ -1,42 +1,42 @@
-let choiceArr = ["Rock", "Paper", "Scissors"];
-
-
 
 function computerSelect() {
     return Math.floor(Math.random() * 3);// rock - 0; paper - 1; scissors - 2
 }
 
 function playerSelect() {
-    let playerChoice = prompt("Rock, Paper, Scissors");
+    let playerChoice = prompt(console.log("Rock, Paper, Scissors"));
     let playerChoiceNum;
     if (playerChoice=='rock') playerChoiceNum = 0
         else if (playerChoice=='paper') playerChoiceNum = 1
     else playerChoiceNum = 2;
     return playerChoiceNum;
-    
 }
 
 function winner() {
     let playerChoiceNum = playerSelect();
     let computerSelectNum = computerSelect();
-    if (playerChoiceNum==computerSelectNum) return ("Tie"+ " " + playerChoiceNum + " " +computerSelectNum ) 
-    else if (playerChoiceNum-computerSelectNum == (-2) || playerChoiceNum-computerSelectNum == 1) return ("Player Won " + " " + playerChoiceNum + " " +computerSelectNum)
-    else return ("Computer Won" + " " + playerChoiceNum + " " +computerSelectNum ) 
-
+    if (playerChoiceNum==computerSelectNum) return "Tie"
+    else if (playerChoiceNum-computerSelectNum == (-2) || playerChoiceNum-computerSelectNum == 1) return "1" //playerWin
+    else return "2"
 }
 
 function game() {
-    console.log("game started");
-    let playerChoice = playerSelect();
-    let computerChoice = computerSelect();
-    
-    /*
-    if (playerChoice == computerChoice) console.log("Tie", "Player choise is: " + playerChoice, "Computer choise is: " + computerChoice);
-    if (playerChoice == 'rock' && computerChoice == 'scissors') console.log("Player won", "Player choise is: " + playerChoice, "Computer choise is: " + computerChoice);
-    if (playerChoice == "paper" && computerChoice == 'rock') console.log("Player won", "Player choise is: " + playerChoice, "Computer choise is: " + computerChoice);
-    if (playerChoice == 'scissors' && computerChoice == 'paper') console.log("Player won", "Player choise is: " + playerChoice, "Computer choise is: " + computerChoice);
-    else console.log("Computer won", "Player choise is: " + playerChoice, "Computer choise is: " + computerChoice);
-    */
-    
+    let playerWin = 0;
+    let computerWin =0;
+    let ties=0;
+    for (let i=0;i<5;i++) {
+        switch (winner()) {
+            case "Tie":
+                ties++;
+                break;
+            case "1":
+                playerWin++;
+                break;
+            case "2":
+                computerWin++;
+                break;
+        }
+    }
+    return (console.log("Player score "+playerWin + " Computer score "+computerWin + " Ties " + ties))   
 }
 
