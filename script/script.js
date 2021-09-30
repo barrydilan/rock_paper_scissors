9972a9cb2ea4f0d25c1712729c68f5351747d706
HarryH <queharambe@gmail.com>
2021-09-30 20:06:55 +0300

    UI101

--- script/script.js
+++ script/script.js
@@ -1,43 +1,71 @@
+let computerScore = 0;
+let playerScore = 0;
 
-function computerSelect() {
-    return Math.floor(Math.random() * 3);// rock - 0; paper - 1; scissors - 2
+function getComputeChoice() {
+    let computerChoice =  Math.floor(Math.random() * 3); // rock - 0; paper - 1; scissors - 2
+    return computerChoice;
 }
 
-function playerSelect() {
-    let playerChoice = prompt(console.log("Rock, Paper, Scissors"));
-    let playerChoiceNum;
-    if (playerChoice=='rock') playerChoiceNum = 0
-        else if (playerChoice=='paper') playerChoiceNum = 1
-    else playerChoiceNum = 2;
-    return playerChoiceNum;
+function getPlayerChoice(e){
+    let playerChoice = e.target.id;
+    if (playerChoice=='rock') playerChoice = 0
+        else if (playerChoice=='paper') playerChoice = 1
+    else playerChoice = 2;
+    playRound(playerChoice)
 }
 
-function winner() {
-    let playerChoiceNum = playerSelect();
-    let computerSelectNum = computerSelect();
-    if (playerChoiceNum==computerSelectNum) return "Tie"
-    else if (playerChoiceNum-computerSelectNum == (-2) || playerChoiceNum-computerSelectNum == 1) return "1" //playerWin
-    else return "2"
+
+function playRound(playerChoice){
+    let computerChoice = getComputeChoice();
+    getWinner(computerChoice,playerChoice);
 }
 
-function game() {
-    let playerWin = 0;
-    let computerWin =0;
-    let ties=0;
-    for (let i=0;i<5;i++) {
-        switch (winner()) {
-            case "Tie":
-                ties++;
-                break;
-            case "1":
-                playerWin++;
-                break;
-            case "2":
-                computerWin++;
-                break;       
+function getWinner(computerChoice, playerChoice){
+    const results = ["Rock", "Paper", "Scissors"]
+    if (playerChoice==computerChoice) updateStatus("Draw: " + results[playerChoice] + " vs " + results[computerChoice]);
+    else if (playerChoice-computerChoice == (-2) || playerChoice-computerChoice == 1) {playerScore++; updateStatus("Player wins: " + results[playerChoice] + " vs " + results[computerChoice]);}                                                       //playerWin
+    else {computerScore++ ; updateStatus("Computer wins: " + results[playerChoice] + " vs " + results[computerChoice])};    //computer win
+    updateScore(playerScore,computerScore);
+}
+
+
+function updateScore(playerScore,computerScore){ ///updates score to DOM
+    playerScore_span.textContent = playerScore;
+    computerScore_span.textContent = computerScore;
+    /* if (playerScore>computerScore) console.log("playr win") */
+    checkRoundsCap(playerScore, computerScore);
+}
+
+function checkRoundsCap(playerScore, computerScore){ /// check winner after 5 rounds
+    if (playerScore+computerScore == 5) {
+        choices.forEach(choice => choice.removeEventListener('click', getPlayerChoice))
+            if (playerScore-computerScore>0) updateStatus("Player Wins!");
+            else if (playerScore-computerScore<0) updateStatus("Computer Wins!");
+            else updateStatus("Draw");
         }
-        console.log("Player score "+playerWin + " Computer score "+computerWin + " Ties " + ties)
     }
-    return (console.log("Player score "+playerWin + " Computer score "+computerWin + " Ties " + ties))   
+
+
+function newGame(){
+    playerScore = 0;
+    computerScore = 0;
+    updateScore(playerScore, computerScore);
+    updateStatus("Choose your hand!")
+    choices.forEach(choice => choice.addEventListener('click', getPlayerChoice))
 }
+
+function updateStatus(msg){
+     statusMessage.textContent = (msg);
+}
+
+
+
+const statusMessage = document.querySelector(".statusMessage");
+const newGameBtn = document.querySelector("#ngbtn")
+newGameBtn.addEventListener('click', newGame);
+const choices = Array.from(document.querySelectorAll('.rps'));
+choices.forEach(choice => choice.addEventListener('click', getPlayerChoice))
+const playerScore_span = document.querySelector("#playerScore")
+const computerScore_span = document.querySelector("#computerScore")
+const tieScore = document.querySelector("#tie")
 
